b641b6a69f2c426e250781557d815f79744965dd
Zac Thamer <zactham@gmail.com>
2017-07-05 21:38:59 -0400

    Clean Up

--- src/Square.java
+++ src/Square.java
@@ -3,10 +3,10 @@ import java.awt.Graphics;
 
 public class Square
 {
-	public int x;
-	public int y;
-	public int size;//size of the sides of the squares
-	public Color c;//color of the square
+	private int x;
+	private int y;
+	private int size;//size of the sides of the squares
+	private Color c;//color of the square
 
 	public Square(int xCoord, int yCoord, int squareSize, Color col)
 	{
@@ -16,22 +16,22 @@ public class Square
 		c = col;
 	}
 
-	
+
 	public int getX()
 	{
 		return x;
 	}
-	
+
 	public int getY()
 	{
 		return y;
 	}
-	
+
 	public void setX(int newX)
 	{
 		x = newX;
 	}
-	
+
 	public void setY(int newY)
 	{
 		y = newY;
@@ -48,7 +48,7 @@ public class Square
 	{
 		return true;
 	}
-	
+
 	public void moveTo(Square otherSq)
 	{
 		this.setX(otherSq.getX());
