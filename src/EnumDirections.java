7855b4121b3adf3128297187cb903d25a8a8724f
mus <mus@thamers.com>
2017-07-17 21:46:33 -0400

    enum

--- src/EnumDirections.java
+++ src/EnumDirections.java
@@ -1,25 +1,4 @@
-public class EnumDirections
+public enum EnumDirections
 {
-	public enum Direction
-	{
-		UP, DOWN, RIGHT, LEFT
-	}
-	
-	Direction d;
-	
-		public EnumDirections(Direction directionsInput)
-		{
-			d = directionsInput;
-		}
-		
-		public Direction getEnumDirection()
-		{
-			return d;
-		}
-		
-		public void setEnumDirection(Direction directionsInput)
-		{
-			d = directionsInput;
-		}
-	
+	UP, DOWN, RIGHT, LEFT
 }
